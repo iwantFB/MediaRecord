bb850d63c1f7822bd504f4188186dff2f49c512c
jackson <jackson.h@walnuttech.co>
2018-11-22 18:19:47 +0800

    断点录制未完成，代码需要优化

--- FHMediaRecord/FHMediaRecord/ViewController.m
+++ FHMediaRecord/FHMediaRecord/ViewController.m
@@ -8,6 +8,12 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+#define VIDEO_FILEPATH @"video"
+#define ScreenWidth [UIScreen mainScreen].bounds.size.width
+#define ScreenHeight [UIScreen mainScreen].bounds.size.height
+#define iSiPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
 
 ///用来修改某个输入设备
 typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
@@ -25,14 +31,13 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
 
 @property (strong, nonatomic) AVCaptureStillImageOutput *captureStillImageOutput;        //照片输出流
 
+@property (nonatomic, assign) UIDeviceOrientation shootingOrientation;
 
 @property (nonatomic, strong) AVAssetWriter *assetWriter;
 @property (nonatomic, strong) AVAssetWriterInput *assetWriterVideoInput;
 @property (nonatomic, strong) AVAssetWriterInput *assetWriterAudioInput;
 @property (nonatomic, strong) NSDictionary *videoCompressionSettings;
 @property (nonatomic, strong) NSDictionary *audioCompressionSettings;
-
-@property (strong, nonatomic) NSURL *videoURL;
 
 @end
 
@@ -44,13 +49,18 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
     AVCaptureAudioDataOutput *_audioDataOutput;
     AVCaptureVideoDataOutput *_videoDataOutput;
     
+    NSURL *_fileURL;
+    
     CMTime _startTimestamp;
     CMTime _timeOffset;
     CMTime _maximumCaptureDuration;
+    CMTime _lastVideo;
+    CMTime _lastAudio;
     
     BOOL _isRotating;
     BOOL _isPaused;
     BOOL _isVideoRecord;
+    BOOL _interrupted;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -69,17 +79,29 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
 
 - (IBAction)pauseRecord:(id)sender {
     
-    
+    _interrupted = YES;
+    _isPaused = YES;
 }
 
 - (IBAction)resumeRecoed:(id)sender {
-    
+    _isPaused = NO;
     
 }
 
 - (IBAction)changeFlash:(id)sender {
     
-    
+    __weak AVAssetWriter *weakWriter = _assetWriter;
+    __weak ViewController *weakSelf = self;
+    [_assetWriter finishWritingWithCompletionHandler:^{
+        if(weakWriter.error){
+            NSLog(@"看看%@",weakWriter.error.localizedDescription);
+        }else{
+            AVPlayerViewController *mpVC = [];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [weakSelf presentViewController:mpVC animated:YES completion:nil];
+            });
+        }
+    }];
 }
 
 - (IBAction)changeCamera:(id)sender {
@@ -120,23 +142,69 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
     if(!_assetWriter)return;
     
     @autoreleasepool {
-     
-        if(connection == [_videoDataOutput connectionWithMediaType:AVMediaTypeVideo]){
+        
+        CMTime currentTimestamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
+        BOOL isVideo = (output == _videoDataOutput);
+        ///暂停过，计算时间偏差
+        if(_interrupted){
+            if (isVideo) {
+                
+                return;
+            }
             
+            // calculate the appropriate time offset
+            if (CMTIME_IS_VALID(currentTimestamp) && CMTIME_IS_VALID(_lastAudio)) {
+                if (CMTIME_IS_VALID(_timeOffset)) {
+                    currentTimestamp = CMTimeSubtract(currentTimestamp, _timeOffset);
+                }
+                
+                CMTime offset = CMTimeSubtract(currentTimestamp, _lastAudio);
+                _timeOffset = CMTIME_IS_INVALID(_timeOffset) ? offset : CMTimeAdd(_timeOffset, offset);
+            }
+            _interrupted = NO;
         }
         
         
-        [self _automaticallyEndCaptureIfMaximumDurationReachedWithSampleBuffer:sampleBuffer];
+        if (_timeOffset.value > 0) {
+            
+            //根据得到的timeOffset调整
+            sampleBuffer = [self adjustTime:sampleBuffer by:_timeOffset];
+        }
         
+        CMTime pts = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
+        CMTime dur = CMSampleBufferGetDuration(sampleBuffer);
+        if (dur.value > 0) {
+            pts = CMTimeAdd(pts, dur);
+        }
+        if (isVideo) {
+            _lastVideo = pts;
+        }else {
+            _lastAudio = pts;
+        }
     }
+    
+    
+    if(connection == [_videoDataOutput connectionWithMediaType:AVMediaTypeVideo]){
+        [self encodeFrame:sampleBuffer isVideo:YES];
+    }
+    
+    
+    [self _automaticallyEndCaptureIfMaximumDurationReachedWithSampleBuffer:sampleBuffer];
+    
 }
 
 #pragma mark- private method
 - (void)_configurationData
 {
     _startTimestamp = CMClockGetTime(CMClockGetHostTimeClock());
-    _timeOffset = kCMTimeInvalid;
-    _maximumCaptureDuration = kCMTimeInvalid;
+    _timeOffset = kCMTimeZero;
+    Float64 duration = 60;
+    _maximumCaptureDuration = CMTimeMakeWithSeconds(duration, 600);
+    
+    _fileURL = [self createAssetFileURL];
+    _assetWriter = [AVAssetWriter assetWriterWithURL:_fileURL fileType:AVFileTypeMPEG4 error:nil];
+    
+    [self _configWriteVideoInput];
     
     [self changePositionCameraForDeviceInput:self.backCameraInput];
     
@@ -166,6 +234,56 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
     _previewLayer.frame = [UIScreen mainScreen].bounds;
     
     [_captureSession startRunning];
+}
+
+- (void)_configWriteVideoInput{
+    //写入视频大小
+    NSInteger numPixels = ScreenWidth * ScreenHeight;
+    
+    //每像素比特
+    CGFloat bitsPerPixel = 12.0;
+    NSInteger bitsPerSecond = numPixels * bitsPerPixel;
+    
+    // 码率和帧率设置
+    NSDictionary *compressionProperties = @{ AVVideoAverageBitRateKey : @(bitsPerSecond),
+                                             AVVideoExpectedSourceFrameRateKey : @(15),
+                                             AVVideoMaxKeyFrameIntervalKey : @(15),
+                                             AVVideoProfileLevelKey : AVVideoProfileLevelH264BaselineAutoLevel };
+    CGFloat width = ScreenHeight;
+    CGFloat height = ScreenWidth;
+    if (iSiPhoneX)
+    {
+        width = ScreenHeight - 146;
+        height = ScreenWidth;
+    }
+    //视频属性
+    self.videoCompressionSettings = @{ AVVideoCodecKey : AVVideoCodecH264,
+                                       AVVideoWidthKey : @(width * 2),
+                                       AVVideoHeightKey : @(height * 2),
+                                       AVVideoScalingModeKey : AVVideoScalingModeResizeAspectFill,
+                                       AVVideoCompressionPropertiesKey : compressionProperties };
+    
+    _assetWriterVideoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:self.videoCompressionSettings];
+    //expectsMediaDataInRealTime 必须设为yes，需要从capture session 实时获取数据
+    _assetWriterVideoInput.expectsMediaDataInRealTime = YES;
+    
+    if (self.shootingOrientation == UIDeviceOrientationLandscapeRight)
+    {
+        _assetWriterVideoInput.transform = CGAffineTransformMakeRotation(M_PI);
+    }
+    else if (self.shootingOrientation == UIDeviceOrientationLandscapeLeft)
+    {
+        _assetWriterVideoInput.transform = CGAffineTransformMakeRotation(0);
+    }
+    else if (self.shootingOrientation == UIDeviceOrientationPortraitUpsideDown)
+    {
+        _assetWriterVideoInput.transform = CGAffineTransformMakeRotation(M_PI + (M_PI / 2.0));
+    }
+    else
+    {
+        _assetWriterVideoInput.transform = CGAffineTransformMakeRotation(M_PI / 2.0);
+    }
+    
 }
 
 - (void)changeDeviceProperty:(PropertyChangeBlock)propertyChange
@@ -206,6 +324,134 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
         }
     }
 }
+
+//调整媒体数据的时间
+- (CMSampleBufferRef)adjustTime:(CMSampleBufferRef)sample by:(CMTime)offset {
+    CMItemCount count;
+    CMSampleBufferGetSampleTimingInfoArray(sample, 0, nil, &count);
+    CMSampleTimingInfo* pInfo = malloc(sizeof(CMSampleTimingInfo) * count);
+    CMSampleBufferGetSampleTimingInfoArray(sample, count, pInfo, &count);
+    for (CMItemCount i = 0; i < count; i++) {
+        pInfo[i].decodeTimeStamp = CMTimeSubtract(pInfo[i].decodeTimeStamp, offset);
+        pInfo[i].presentationTimeStamp = CMTimeSubtract(pInfo[i].presentationTimeStamp, offset);
+    }
+    CMSampleBufferRef sout;
+    CMSampleBufferCreateCopyWithNewTiming(nil, sample, count, pInfo, &sout);
+    free(pInfo);
+    return sout;
+}
+
+- (BOOL)encodeFrame:(CMSampleBufferRef) sampleBuffer isVideo:(BOOL)isVideo {
+    //数据是否准备写入
+    if (CMSampleBufferDataIsReady(sampleBuffer)) {
+        //写入状态为未知,保证视频先写入
+        if (_assetWriter.status == AVAssetWriterStatusUnknown && isVideo) {
+            //获取开始写入的CMTime
+            CMTime startTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
+            //开始写入
+            [_assetWriter startWriting];
+            [_assetWriter startSessionAtSourceTime:startTime];
+        }
+        //写入失败
+        if (_assetWriter.status == AVAssetWriterStatusFailed) {
+            NSLog(@"writer error %@", _assetWriter.error.localizedDescription);
+            return NO;
+        }
+        //判断是否是视频
+        if (isVideo) {
+            //视频输入是否准备接受更多的媒体数据
+            if (_assetWriterVideoInput.readyForMoreMediaData == YES) {
+                //拼接数据
+                [_assetWriterVideoInput appendSampleBuffer:sampleBuffer];
+                return YES;
+            }
+        }else {
+            //音频输入是否准备接受更多的媒体数据
+            if (_assetWriterAudioInput.readyForMoreMediaData) {
+                //拼接数据
+                [_assetWriterAudioInput appendSampleBuffer:sampleBuffer];
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+- (NSURL *)createAssetFileURL
+{
+    // 创建视频文件的存储路径
+    NSString *filePath = [self createVideoFolderPath];
+    if (filePath == nil)
+    {
+        return nil;
+    }
+    
+    NSString *videoType = @".mp4";
+    NSString *videoDestDateString = [self createFileNamePrefix];
+    NSString *videoFileName = [videoDestDateString stringByAppendingString:videoType];
+    
+    NSUInteger idx = 1;
+    /*We only allow 10000 same file name*/
+    NSString *finalPath = [NSString stringWithFormat:@"%@/%@", filePath, videoFileName];
+    
+    while (idx % 10000 && [[NSFileManager defaultManager] fileExistsAtPath:finalPath])
+    {
+        finalPath = [NSString stringWithFormat:@"%@/%@_(%lu)%@", filePath, videoDestDateString, (unsigned long)idx++, videoType];
+    }
+    
+    
+    return [NSURL fileURLWithPath:finalPath];
+}
+
+- (NSString *)createVideoFolderPath
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *homePath = NSHomeDirectory();
+    
+    NSString *tmpFilePath;
+    
+    if (homePath.length > 0)
+    {
+        NSString *documentPath = [homePath stringByAppendingString:@"/Documents"];
+        if ([fileManager fileExistsAtPath:documentPath isDirectory:NULL] == YES)
+        {
+            BOOL success = NO;
+            
+            NSArray *paths = [fileManager contentsOfDirectoryAtPath:documentPath error:nil];
+            
+            //offline file folder
+            tmpFilePath = [documentPath stringByAppendingString:[NSString stringWithFormat:@"/%@", VIDEO_FILEPATH]];
+            if ([paths containsObject:VIDEO_FILEPATH] == NO)
+            {
+                success = [fileManager createDirectoryAtPath:tmpFilePath withIntermediateDirectories:YES attributes:nil error:nil];
+                if (!success)
+                {
+                    tmpFilePath = nil;
+                }
+            }
+            return tmpFilePath;
+        }
+    }
+    
+    return false;
+}
+
+/**
+ *  创建文件名
+ */
+- (NSString *)createFileNamePrefix
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss zzz"];
+    
+    NSString *destDateString = [dateFormatter stringFromDate:[NSDate date]];
+    destDateString = [destDateString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+    destDateString = [destDateString stringByReplacingOccurrencesOfString:@"+" withString:@"-"];
+    destDateString = [destDateString stringByReplacingOccurrencesOfString:@":" withString:@"-"];
+    
+    return destDateString;
+}
+
 
 #pragma mark- setter/getter
 - (dispatch_queue_t)videoQueue
